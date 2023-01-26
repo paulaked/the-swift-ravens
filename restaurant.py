2b7855f8b58383a278ef2051e520c39e3975658d
paulaked <47561863+paulaked@users.noreply.github.com>
2023-01-26 16:23:40 +0000

    Add files via upload

---
+++ restaurant.py
@@ -0,0 +1,18 @@
+class Table:
+    def __init__(self):
+        pass
+
+    def order(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_subtotal(self):
+        pass
+
+    def get_total(self):
+        pass
+
+    def split_bill(self):
+        pass
