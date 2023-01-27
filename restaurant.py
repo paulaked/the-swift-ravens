40eb0774825d28caec7293b39794d30dda1b0117
TasDevelops <tasnimmohamed471@gmail.com>
2023-01-27 18:36:58 +0000

    Needs more work to pass the test cases

--- restaurant.py
+++ restaurant.py
@@ -1,18 +1,45 @@
 class Table:
-    def __init__(self):
-        pass
+    def __init__(self, table_num):
+        self.bill = []
+        self.subtotal= 0
+        self.table_num = table_num
+        
 
-    def order(self):
-        pass
+    def order(self, item, price, quantity = 1):
+        order_list = {"item": item, "price": price, "quantity": quantity}
+        self.item = item
+        self.bill.append (order_list)
+        return self.bill
 
-    def remove(self):
-        pass
+
+    def remove(self, item, price, quantity):
+        old_quantity = self.bill[0]['quantity']
+        new_quantity = old_quantity - quantity
+        self.bill[0]['quantity'] = new_quantity
 
     def get_subtotal(self):
-        pass
+        for num in self.bill:
+            self.subtotal += num['price'] * num['quantity']
+        print(self.subtotal)
+        return self.subtotal
+        
 
-    def get_total(self):
-        pass
+    def get_total(self, discount):
+        discount_price = self.subtotal * discount
+        total_price = self.subtotal + discount_price
+        print ({'Sub Total': self.subtotal, 'Service Charge': discount_price, 'Total': total_price})
+        return {'Sub Total': self.subtotal, 'Service Charge': discount_price, 'Total': total_price}
+
 
     def split_bill(self):
-        pass
+        bill_split = self.total_price / self.guests_per_table
+        print(bill_split)
+
+
+tab = Table(2)
+tab.order('egg', 10.00, 3)
+tab.order('Food', 10.00, 5)
+tab.order('Food', 5, 1)
+print(tab.get_subtotal())
+print(tab.get_total(0.15))
+print(tab.split_bill)()
